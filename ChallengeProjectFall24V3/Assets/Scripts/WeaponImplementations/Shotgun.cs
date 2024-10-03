e6a6097a755bf5435c6c185ae81f2b234d2a608a
lennart3211 <82011287+lennart3211@users.noreply.github.com>
2024-10-02 21:45:17 -0500

    Added shotgun shooting animation

--- ChallengeProjectFall24V3/Assets/Scripts/WeaponImplementations/Shotgun.cs
+++ ChallengeProjectFall24V3/Assets/Scripts/WeaponImplementations/Shotgun.cs
@@ -1,14 +1,13 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class Shotgun : MonoBehaviour, IWeapon
 {
     [SerializeField] private LayerMask enemyLayer;
-    private Animator gunAnim;
+    [SerializeField] private Animator gunAnim;
+    [SerializeField] private float fireRate;
+    [SerializeField] AudioClip shootSFX;
+    private AudioSource gunSource;
     public Transform camTransform;
     public ShotgunPellet pelletPrefab;
 
@@ -16,29 +15,39 @@ public class Shotgun : MonoBehaviour, IWeapon
     [SerializeField] private uint _numPellets = 5;
     [SerializeField] private float _pelletSpeed = 50f;
     [SerializeField] private float _spreadAngle = 15f;
+    
 
-    private uint _numPelletsSqrt;
+    private uint numPelletsSqrt;
+    private float shootTimer;
 
     private void Start()
     {
         gunAnim = GetComponent<Animator>();
         camTransform = Camera.main.transform;
-        _numPelletsSqrt = (uint)Mathf.Sqrt(_numPellets);
+        numPelletsSqrt = (uint)Mathf.Sqrt(_numPellets);
     }
 
     public void Shoot()
     {
+        
+        if (shootTimer > 0) return;
+        
+        gunAnim.Play("ShotgunShoot");
+
+        // gunSource.PlayOneShot(shootSFX);
+        // gunAnim.Play("Shoot");
+        
         gunAnim.SetTrigger("Shoot");
 
         Vector3 offset = new Vector3(0, 0, 0);
 
         Vector3 muzzlePosition = transform.TransformPoint(_muzzleOffset);
-        for (uint i = 0; i < _numPelletsSqrt; i++)
+        for (uint i = 0; i < numPelletsSqrt; i++)
         {
-            for (uint j = 0; j < _numPelletsSqrt; j++)
+            for (uint j = 0; j < numPelletsSqrt; j++)
             {
-                offset.x = i * 0.1f;
-                offset.y = j * 0.1f;
+                offset.x = i * 0.02f;
+                offset.y = j * 0.02f;
                 ShotgunPellet pellet = Instantiate(pelletPrefab, muzzlePosition + offset, Quaternion.identity);
             
                 Vector3 spreadDirection = Random.insideUnitSphere * Mathf.Tan(_spreadAngle * Mathf.Deg2Rad);
@@ -48,11 +57,12 @@ public class Shotgun : MonoBehaviour, IWeapon
                 pellet.GetComponent<Rigidbody>().velocity = shootDirection * _pelletSpeed;
             }
         }
+
+        shootTimer = fireRate;
     }
 
     private void Update()
     {
-        //shooting visualizer
-        // Debug.DrawRay(camTransform.position, camTransform.forward * 10000f, Color.red);
+        shootTimer -= Time.deltaTime;
     }
 }
--- ChallengeProjectFall24V3/Assets/Scripts/WeaponImplementations/ShotgunPellet.cs
+++ ChallengeProjectFall24V3/Assets/Scripts/WeaponImplementations/ShotgunPellet.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class ShotgunPellet : MonoBehaviour
 {
-    [SerializeField] private float _lifetime = 5f;
+    [SerializeField] private float lifetime = 5f;
 
     private void Start()
     {
-        Destroy(gameObject, _lifetime);
+        Destroy(gameObject, lifetime);
     }
 
     private void OnCollisionEnter(Collision collision)
