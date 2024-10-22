554134e7346d112960044448e9b721d8c0062197
Jake Ozer <jakobozer1@gmail.com>
2024-10-21 21:59:22 -0500

    pause stuff working good

---
+++ ChallengeProjectFall24V3/Assets/Scripts/GeneralGameFunctions/PauseMenu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    private MainInput input;
+    private bool paused = false;
+    [SerializeField] private GameObject menuObj;
+    private GameObject crosshairGO;
+
+    private void Awake()
+    {
+        input = new MainInput();
+        menuObj.SetActive(false);
+        crosshairGO = GameObject.Find("Crosshair");
+    }
+
+    // --------- enable/disbale input when script toggled on/off -------------- 
+    private void OnEnable()
+    {
+        input.Enable();
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+    }
+    // ------------------------Do not touch this------------------------------
+
+    private void Update()
+    {
+        if (input.Ground.Pause.triggered)
+        {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause()
+    {
+        paused = !paused;
+        menuObj.SetActive(paused);
+        FindObjectOfType<PlayerShoot>().enabled = !paused;
+        Cursor.lockState = paused ? CursorLockMode.None : CursorLockMode.Locked;
+        Cursor.visible = paused;
+        crosshairGO.SetActive(!paused);
+        Time.timeScale = paused ? 0 : 1;
+    }
+}
