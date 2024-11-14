30b060bb11461b0981fab9bbc272d43a47448464
maalikjhopkins <89146299+maalikjhopkins@users.noreply.github.com>
2024-11-13 22:06:18 -0600

    Good

--- ChallengeProjectFall24V3/Assets/Scripts/WeaponImplementations/Knife.cs
+++ ChallengeProjectFall24V3/Assets/Scripts/WeaponImplementations/Knife.cs
@@ -4,26 +4,5 @@ using UnityEngine;
 
 public class Knife : MonoBehaviour
 {
-    private float timer;
-    private Animator anim;
-    // Start is called before the first frame update
-    void Start()
-    {
-        timer = 4;
-        anim = GetComponent<Animator>();
-    }
 
-    // Update is called once per frame
-    void Update()
-    {   
-        if (input.GetKeyDown(KeyCode.v))
-        {
-            Swing();
-        }
-    }
-
-    private void Swing()
-    {
-
-    }
 }
